6db25f4ae708fcb4342e1a22586e725c312f3355
Luis Sancho <kidbucle100@gmail.com>
2026-04-28 20:31:53 -0500

    Refactor Resume component Button styling
    Removed inline margin from Button component and updated its className.

--- src/scenes/Resume/Resume.jsx
+++ src/scenes/Resume/Resume.jsx
@@ -4,7 +4,6 @@ import Button from '../../components/UIElements/Button/Button';
 import BaseLayout from '../../layouts/BaseLayout/BaseLayout';
 import s from './Resume.module.scss';
 
-// Usar el enlace de vista previa - funciona siempre
 const resumeLink = 'https://drive.google.com/file/d/1UQrRzv4B0_OMxjqFt_kFrcjVRvYeUTsP/view';
 
 const Resume = () => {
@@ -12,6 +11,7 @@ const Resume = () => {
     <BaseLayout>
       <div className={s.content}>
         <div className={s.header}>
+          
           <h1 className={s.title}>
             My <br /> Resume
           </h1>
@@ -20,9 +20,9 @@ const Resume = () => {
             <FilesIcon />
           </div>
 
+          {/* 🔥 quitamos margin auto inline */}
           <Button
-            style={{ margin: 'auto', width: '15rem' }}
-            className="primary"
+            className={`${s.downloadBtn} primary`}
             href={resumeLink}
             target="_blank"
           >
@@ -30,6 +30,7 @@ const Resume = () => {
             <span className={s.downloadText}> download resume</span>
             <span className={s.filename}>.pdf</span>
           </Button>
+
         </div>
       </div>
     </BaseLayout>
