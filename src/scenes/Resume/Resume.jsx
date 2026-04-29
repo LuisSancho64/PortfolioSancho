c9992785812762f6366362854e412da00c8f2af3
Luis Sancho <kidbucle100@gmail.com>
2026-04-28 20:46:03 -0500

    Refactor Resume component with updated button styles
    Updated the Resume component to include corrected button styles and comments.

--- src/scenes/Resume/Resume.jsx
+++ src/scenes/Resume/Resume.jsx
@@ -4,6 +4,7 @@ import Button from '../../components/UIElements/Button/Button';
 import BaseLayout from '../../layouts/BaseLayout/BaseLayout';
 import s from './Resume.module.scss';
 
+// Usar el enlace de vista previa - funciona siempre
 const resumeLink = 'https://drive.google.com/file/d/1UQrRzv4B0_OMxjqFt_kFrcjVRvYeUTsP/view';
 
 const Resume = () => {
@@ -11,7 +12,6 @@ const Resume = () => {
     <BaseLayout>
       <div className={s.content}>
         <div className={s.header}>
-          
           <h1 className={s.title}>
             My <br /> Resume
           </h1>
@@ -20,7 +20,7 @@ const Resume = () => {
             <FilesIcon />
           </div>
 
-          {/* 🔥 SOLO ESTA CLASE */}
+          {/* 🔥 BOTÓN CORREGIDO - Usa la clase de CSS Modules */}
           <Button
             className={s.downloadBtn}
             href={resumeLink}
@@ -30,7 +30,6 @@ const Resume = () => {
             <span className={s.downloadText}>DOWNLOAD RESUME</span>
             <span className={s.filename}>.PDF</span>
           </Button>
-
         </div>
       </div>
     </BaseLayout>
