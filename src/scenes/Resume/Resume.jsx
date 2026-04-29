e108868ae4061311aa26564945862e5a0993fef5
Luis Sancho <kidbucle100@gmail.com>
2026-04-28 20:14:37 -0500

    Change resume download link to direct access
    Updated the resume download link to allow direct downloads from Google Drive.

--- src/scenes/Resume/Resume.jsx
+++ src/scenes/Resume/Resume.jsx
@@ -5,7 +5,7 @@ import BaseLayout from '../../layouts/BaseLayout/BaseLayout';
 import s from './Resume.module.scss';
 
 // URL directa de descarga de Google Drive
-const resumeLink = 'https://drive.google.com/file/d/1UQrRzv4B0_OMxjqFt_kFrcjVRvYeUTsP/view';
+const resumeLink = 'https://drive.google.com/uc?export=download&id=1UQrRzv4B0_OMxjqFt_kFrcjVRvYeUTsP';
 
 const Resume = () => {
   return (
