0fdbc97f0ed0fa366113569e84f5ba71f86a7eaf
Luis Sancho <kidbucle100@gmail.com>
2026-04-28 20:47:44 -0500

    Refactor Resume component button styles and text
    Updated button styles and text for better presentation.

--- src/scenes/Resume/Resume.jsx
+++ src/scenes/Resume/Resume.jsx
@@ -4,7 +4,6 @@ import Button from '../../components/UIElements/Button/Button';
 import BaseLayout from '../../layouts/BaseLayout/BaseLayout';
 import s from './Resume.module.scss';
 
-// Usar el enlace de vista previa - funciona siempre
 const resumeLink = 'https://drive.google.com/file/d/1UQrRzv4B0_OMxjqFt_kFrcjVRvYeUTsP/view';
 
 const Resume = () => {
@@ -12,6 +11,7 @@ const Resume = () => {
     <BaseLayout>
       <div className={s.content}>
         <div className={s.header}>
+          
           <h1 className={s.title}>
             My <br /> Resume
           </h1>
@@ -20,16 +20,17 @@ const Resume = () => {
             <FilesIcon />
           </div>
 
-          {/* 🔥 BOTÓN CORREGIDO - Usa la clase de CSS Modules */}
+          {/* 🔥 quitamos margin auto inline */}
           <Button
-            className={s.downloadBtn}
+            className={`${s.downloadBtn} primary`}
             href={resumeLink}
             target="_blank"
           >
-            <DownloadIcon />
-            <span className={s.downloadText}>DOWNLOAD RESUME</span>
-            <span className={s.filename}>.PDF</span>
+            <DownloadIcon fill="#fff" />
+            <span className={s.downloadText}> download resume</span>
+            <span className={s.filename}>.pdf</span>
           </Button>
+
         </div>
       </div>
     </BaseLayout>
