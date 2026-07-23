c44109ead86c2356216509e85953cc17b7f23cd7
Luis Sancho <kidbucle100@gmail.com>
2026-07-23 17:25:10 -0500

    Change resume download link to direct download
    Updated the resume download link to a direct download URL and added 'download' attribute to the button.

--- src/scenes/Resume/Resume.jsx
+++ src/scenes/Resume/Resume.jsx
@@ -4,8 +4,8 @@ import Button from '../../components/UIElements/Button/Button';
 import BaseLayout from '../../layouts/BaseLayout/BaseLayout';
 import s from './Resume.module.scss';
 
-// Usar el enlace de vista previa - funciona siempre
-const resumeLink = 'https://drive.google.com/file/d/1YE8nwEwkHvyrjgsPo4CJCxVq9dDRLXxp/view';
+// Enlace de descarga directa de Google Drive
+const resumeLink = 'https://drive.google.com/uc?export=download&id=1YE8nwEwkHvyrjgsPo4CJCxVq9dDRLXxp';
 
 const Resume = () => {
   return (
@@ -15,17 +15,17 @@ const Resume = () => {
           <h1 className={s.title}>
             My <br /> Resume
           </h1>
-
           <div className={s.filesImg}>
             <FilesIcon />
           </div>
-
           {/* 🔥 BOTÓN CON ESTILO ORIGINAL - Usando className="primary" como estaba 🔥 */}
           <Button
             style={{ margin: 'auto', width: '15rem' }}
             className="primary"
             href={resumeLink}
+            download="Resume.pdf"
             target="_blank"
+            rel="noopener noreferrer"
           >
             <DownloadIcon fill="#fff" />
             <span className={s.downloadText}>DOWNLOAD RESUME</span>
